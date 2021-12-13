4d76e751ec7ef0a4b931f9c9e83b7ecd5235e4e9
as199 <dioneassane0290@gmail.com>
2021-12-13 08:35:29 +0000

    refresh token

--- migrations/Version20211211080933.php
+++ migrations/Version20211211080933.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20211210101608 extends AbstractMigration
+final class Version20211211080933 extends AbstractMigration
 {
     public function getDescription(): string
     {
